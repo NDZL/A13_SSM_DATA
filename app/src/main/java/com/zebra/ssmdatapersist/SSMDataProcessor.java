e3cf02ed3f3a2ffe223adc5b8135a1f793a01e65
CXNT48 <cxnt48@zebra.com>
2022-11-11 13:01:13 +0100

    massive insert added

--- app/src/main/java/com/zebra/ssmdatapersist/SSMDataProcessor.java
+++ app/src/main/java/com/zebra/ssmdatapersist/SSMDataProcessor.java
@@ -12,15 +12,17 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//test for branch benchmark
+import java.util.Random;
+import java.util.UUID;
+
+//the  benchmark branch aims at heavily testing the SSM performance
 
 public class SSMDataProcessor extends AppCompatActivity {
 
@@ -28,6 +30,8 @@ public class SSMDataProcessor extends AppCompatActivity {
     ContentProviderClient cpClient;
     private final String AUTHORITY = "content://com.zebra.securestoragemanager.securecontentprovider/data";
     static final String TAG = "SSMDataProcessor";
+    int howManyRecords=10;
+    long seed=3003;
 
     private final String COLUMN_ORIG_APP_PACKAGE = "orig_app_package";
     private final String COLUMN_TARGET_APP_PACKAGE = "target_app_package";
@@ -42,13 +46,15 @@ public class SSMDataProcessor extends AppCompatActivity {
     LocalContentObserver myContentObserver;
     LocalDataSetObserver myDataSetObserver;
     Spinner persisFlagSpinner;
+    Spinner howManyRecsSpinner;
+    Spinner seedSpinner;
     Context mContext = null;
 
     // NDZL DEBUG SIGNATURE ON AS-DELL
     private final String signature = "MIIC5DCCAcwCAQEwDQYJKoZIhvcNAQEFBQAwNzEWMBQGA1UEAwwNQW5kcm9pZCBEZWJ1ZzEQMA4GA1UECgwHQW5kcm9pZDELMAkGA1UEBhMCVVMwIBcNMjIxMDEyMDk1NjM5WhgPMjA1MjEwMDQwOTU2MzlaMDcxFjAUBgNVBAMMDUFuZHJvaWQgRGVidWcxEDAOBgNVBAoMB0FuZHJvaWQxCzAJBgNVBAYTAlVTMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAha0WrWTMN8Yh40qZ9dX9f2FvyWwpdX8T0L8khTCs6bydqbKVSQOoe28g2y9uA5lbhor4nRstJwVY39TKC6v3ESQHIw2ESxoxU6oMVyqxKlOw48mW3fBGVH8A220Zm0Yo4ibmH4E61ahg5sbdxq2cfoizxCfDuRE7+78/kn/na6CdTH9vBlJ8MJpv587jsV78OI7+vT7bnd7PRmx8D3vxsEfdw0BzPA/C+hovy3y2jMFUe36wXZEn4hdIxqAIngeemFabEyAj5ViSvX6LcPdgUmlcrTyapz0QkjpJHrvOkBXwtwCntAESvVIJHkYnZHgMLSXml6MLlklybQGzGOrPRQIDAQABMA0GCSqGSIb3DQEBBQUAA4IBAQAE80M6+8TrJW/74A1DFkdE21ZetggUc47WG1U5R5HBw+6BLdHy/MZtyN1H9eL3TIICPuL4QXR6BCEp/iWzRwjukopwmwFhzCo2IgKmQpidkaFSdLutETwtp04L3PaXjbVxeGkhMVkYDjtbB6xbZx/ioShQ+bKvbmNOQxNdktyCvcx7s8BhzWtcPPmzYSFt0DEk2n4br2yWf9VUQBKgbjJpo/yoKWrCbb4Wu/WtHGOXGNy2r0FLkiocWHL7liGtAN+rpo0wRZtPoPYxxikqUY+ZOu4rXDu1WeLgbrpJjT84PKO/BJ8zfTD0F2nGZZaz3HjBikEjXxsoziZ/axBdfhmJ";
 
 
-
+    static double totalTimesec=0;
     TextView resultView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,12 +63,17 @@ public class SSMDataProcessor extends AppCompatActivity {
          mContext = this.getApplicationContext();
          resultView = findViewById(R.id.result);
 
+        initializeHowManyRecsSpinner();
         initializePersistFlagSpinner();
+        initializeSeedSpinner();
+
         cpUri = Uri.parse(AUTHORITY);
         cpClient = getContentResolver().acquireContentProviderClient(cpUri);
 
         myContentObserver = new LocalContentObserver(null);
         myDataSetObserver = new LocalDataSetObserver();
+
+        resultView.setText("SSM contains "+ ssm_notpersisted_countRecords()+ " not persisted records\nand "+ssm_ispersisted_countRecords()+" persisted records from this app");
     }
 
     private void initializePersistFlagSpinner() {
@@ -74,42 +85,159 @@ public class SSMDataProcessor extends AppCompatActivity {
     }
 
 
+    private void initializeHowManyRecsSpinner() {
+        howManyRecsSpinner = (Spinner)findViewById(R.id.howmanyrecsSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.howmanyrec_flag, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        howManyRecsSpinner.setAdapter(adapter);
+
+        howManyRecsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                howManyRecords = Integer.parseInt(howManyRecsSpinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+    }
+
+
+    private void initializeSeedSpinner() {
+        seedSpinner = (Spinner)findViewById(R.id.seedSpinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.seeds, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seedSpinner.setAdapter(adapter);
+
+        seedSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if(seedSpinner.getSelectedItem().toString().equals("Random")){
+                    seed = System.currentTimeMillis();
+                }
+                else
+                    seed = Integer.parseInt(seedSpinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+
+        });
+    }
+
     public void onClickInsertData(View view) {
         insertData();
     }
 
     public void insertData() {
         try {
-            insertUserNameAndPassword();
+            ssm_insertMassiveData();
         } catch (Exception e) {
             Log.d(TAG, "Insert - error: " + e.getMessage());
         }
     }
 
-    private void insertUserNameAndPassword() {
-        String username = ((EditText)findViewById(R.id.et_insertUserName)).getText().toString();
-        String password = ((EditText)findViewById(R.id.et_insertPassword)).getText().toString();
+
+    long timeBegin=0;
+    private void ssm_insertMassiveData() {
+        String key = "";
+        String val = "";
         ContentValues values = new ContentValues();
 
+        //KEYs are generated in a random and repeatable sequence
+        if(seedSpinner.getSelectedItem().toString().equals("Random")){
+            seed = System.currentTimeMillis();
+        }
+        Random rand = new Random( seed );
 
-        //IT LOOKS LIKE INSERT WORKED WITH A WRONG SIGNATURE TOO...
-        values.put(COLUMN_TARGET_APP_PACKAGE, "{\"pkgs_sigs\":[{\"pkg\":\""+ mContext.getPackageName() +"\",\"sig\":\"" + signature + "\"}]}");
-        values.put(COLUMN_DATA_NAME, fieldname);
-        values.put(COLUMN_DATA_VALUE, "[{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}]");
-        values.put(COLUMN_DATA_TYPE,"1");
-        values.put(COLUMN_DATA_INPUT_FORM, "1");
-        values.put(COLUMN_DATA_OUTPUT_FORM, "1");
-        values.put(COLUMN_DATA_PERSIST_REQUIRED, persisFlagSpinner.getSelectedItem().toString());
-        values.put(COLUMN_MULTI_INSTANCE_REQUIRED, "false");
+        int insert_success_count=0;
+        int insert_error_count=0;
+        timeBegin= System.currentTimeMillis();
+        for(int idx = 0; idx<howManyRecords; idx++){
+            key = ""+rand.nextLong();
+            val = UUID.randomUUID().toString();
+            String mustPersist=persisFlagSpinner.getSelectedItem().toString();
 
-        Uri createdRow = getContentResolver().insert(cpUri, values);
-        Log.d(TAG, "Row Created : " + createdRow.toString());
-        Toast.makeText(mContext, "New Record Inserted", Toast.LENGTH_SHORT).show();
-        resultView.setText("Query Result");
+            values.put(COLUMN_TARGET_APP_PACKAGE, "{\"pkgs_sigs\":[{\"pkg\":\""+ mContext.getPackageName() +"\",\"sig\":\"" + signature + "\"}]}");
+            values.put(COLUMN_DATA_NAME, key);
+            values.put(COLUMN_DATA_VALUE, val);
+            values.put(COLUMN_DATA_TYPE,"1");
+            values.put(COLUMN_DATA_INPUT_FORM, "1");
+            values.put(COLUMN_DATA_OUTPUT_FORM, "1");
+            values.put(COLUMN_DATA_PERSIST_REQUIRED, mustPersist );
+            values.put(COLUMN_MULTI_INSTANCE_REQUIRED, "false");
+
+
+            Uri createdRow = null;
+            try {
+                createdRow = getContentResolver().insert(cpUri, values);
+                insert_success_count++;
+                Log.d(TAG, "Row Created : " + createdRow.toString());
+            } catch (Exception e) {
+                insert_error_count++;
+                Log.d(TAG, "Row Create Excp : " + e.getMessage());
+            }
+
+        }
+        long timeEnd = System.currentTimeMillis();
+        totalTimesec = (1. * timeEnd - 1. * timeBegin) / 1000.0;
+
+        //Toast.makeText(mContext, "New Record Inserted", Toast.LENGTH_SHORT).show();
+        resultView.setText("Inserted:"+insert_success_count+" records. Failed to insert:"+ insert_error_count+" records in "+totalTimesec+"s\n"+  "SSM status: "+ ssm_notpersisted_countRecords() +" non-persisted records\n "+ssm_ispersisted_countRecords()+" persisted records");
+    }
+
+
+    int ssm_notpersisted_countRecords() {
+        Uri cpUriQuery = Uri.parse(AUTHORITY + "/[" + mContext.getPackageName() + "]");
+        String selection = COLUMN_TARGET_APP_PACKAGE + " = '" + mContext.getPackageName() + "'" + " AND " + COLUMN_DATA_PERSIST_REQUIRED + " = 'false'" + " AND " + COLUMN_DATA_TYPE + " = '" + "1" + "'";
+
+        int _count=0;
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(cpUriQuery, null, selection, null, null);
+            _count = cursor.getCount();
+        } catch (Exception e) {
+            Log.d(TAG, "Error: " + e.getMessage());
+        }
+        finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return _count;
+    }
+
+    int ssm_ispersisted_countRecords() {
+        Uri cpUriQuery = Uri.parse(AUTHORITY + "/[" + mContext.getPackageName() + "]");
+        String selection = COLUMN_TARGET_APP_PACKAGE + " = '" + mContext.getPackageName() + "'" + " AND " + COLUMN_DATA_PERSIST_REQUIRED + " = 'true'" + " AND " + COLUMN_DATA_TYPE + " = '" + "1" + "'";
+
+        int _count=0;
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(cpUriQuery, null, selection, null, null);
+            _count = cursor.getCount();
+        } catch (Exception e) {
+            Log.d(TAG, "Error: " + e.getMessage());
+        }
+        finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
+        return _count;
     }
 
     @SuppressLint("Range")
-    public void onClickQueryData(View view) {
+    public void onClickQueryAllDataAtOnce(View view) {
         Uri cpUriQuery = Uri.parse(AUTHORITY + "/[" + mContext.getPackageName() + "]");
         String selection = COLUMN_TARGET_APP_PACKAGE + " = '" + mContext.getPackageName() + "'" + " AND " + COLUMN_DATA_PERSIST_REQUIRED + " = '" + persisFlagSpinner.getSelectedItem().toString() + "'" +
                 " AND " + COLUMN_DATA_TYPE + " = '" + "1" + "'";
@@ -124,19 +252,22 @@ public class SSMDataProcessor extends AppCompatActivity {
             if (cursor != null && cursor.moveToFirst()) {
                 StringBuilder strBuild = new StringBuilder();
                 while (!cursor.isAfterLast()) {
-                    strBuild.append("\n" + "COLUMN_ORIG_APP_PACKAGE :" + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_ORIG_APP_PACKAGE)) + " \n " +
-                            "COLUMN_TARGET_APP_PACKAGE :" + "\n"  + cursor.getString(cursor.getColumnIndex(COLUMN_TARGET_APP_PACKAGE)) + " \n " +
-                            "COLUMN_DATA_NAME: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_NAME)) + " \n " +
-                            "COLUMN_DATA_VALUE: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_VALUE)) + " \n " +
-                            "COLUMN_DATA_TYPE: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_TYPE)) + " \n " +
-                            "COLUMN_DATA_INPUT_FORM: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_INPUT_FORM)) + " \n " +
-                            "COLUMN_DATA_OUTPUT_FORM: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_OUTPUT_FORM)) + " \n " +
-                            "COLUMN_DATA_PERSIST_REQUIRED: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_PERSIST_REQUIRED)) + " \n " +
-                            "COLUMN_MULTI_INSTANCE_REQUIRED: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_MULTI_INSTANCE_REQUIRED)));
+                    strBuild.append("\n" + "" +
+                           // "COLUMN_ORIG_APP_PACKAGE :" + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_ORIG_APP_PACKAGE)) + " \n " +
+                            //"COLUMN_TARGET_APP_PACKAGE :" + "\n"  + cursor.getString(cursor.getColumnIndex(COLUMN_TARGET_APP_PACKAGE)) + " \n " +
+                            "" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_NAME)) + " : " +
+                            "" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_VALUE)) + " \n "+
+                            //"COLUMN_DATA_TYPE: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_TYPE)) + " \n " +
+                            //"COLUMN_DATA_INPUT_FORM: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_INPUT_FORM)) + " \n " +
+                            //"COLUMN_DATA_OUTPUT_FORM: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_OUTPUT_FORM)) + " \n " +
+                            //"COLUMN_DATA_PERSIST_REQUIRED: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_DATA_PERSIST_REQUIRED)) + " \n " +
+                            //"COLUMN_MULTI_INSTANCE_REQUIRED: " + "\n" + cursor.getString(cursor.getColumnIndex(COLUMN_MULTI_INSTANCE_REQUIRED)) +
+                            ""
+                            );
 
                     String targetData = cursor.getString(cursor.getColumnIndex(COLUMN_TARGET_APP_PACKAGE));
-                    Log.d(TAG, "Target Data Received: " + targetData);
-                    strBuild.append("\n ----------------------").append("\n");
+                    //Log.d(TAG, "Target Data Received: " + targetData);
+                    //strBuild.append("\n ----------------------").append("\n");
                     cursor.moveToNext();
                 }
                 Log.d(TAG, "Query data: " + strBuild);
@@ -153,43 +284,21 @@ public class SSMDataProcessor extends AppCompatActivity {
         }
     }
 
-    String fieldname = "usrpass";
-    public void onClickUpdateData(View view) {
-        String username = ((EditText) findViewById(R.id.et_insertUserName)).getText().toString();
-        String password = ((EditText) findViewById(R.id.et_insertPassword)).getText().toString();
-        try {
-            ContentValues values = new ContentValues();
-            values.put(COLUMN_TARGET_APP_PACKAGE, "{\"pkgs_sigs\":[{\"pkg\":\"" + mContext.getPackageName() + "\",\"sig\":\"" + signature + "\"}]}");
-            values.put(COLUMN_DATA_NAME, fieldname);
-            values.put(COLUMN_DATA_VALUE, "[{\"username\":\"" + username + "\",\"password\":\"" + password + "\"}]");
-            values.put(COLUMN_DATA_PERSIST_REQUIRED, persisFlagSpinner.getSelectedItem().toString());
-            int rowNumbers = getContentResolver().update(cpUri, values, null, null);
-            Log.d(TAG, "Records updated: " + rowNumbers);
-            if (rowNumbers > 0) {
-                Toast.makeText(mContext, "User info updated", Toast.LENGTH_SHORT).show();
-                resultView.setText("Query Result");
-            } else {
-                Toast.makeText(mContext, "No records to update", Toast.LENGTH_SHORT).show();
-            }
-        } catch (Exception e) {
-            Log.d(TAG, "Update - error: " + e.getMessage());
-            Toast.makeText(mContext, "Error updating User info " + e.getMessage(), Toast.LENGTH_SHORT).show();
 
-        }
-    }
 
-    public void onClickDeleteData(View view) {
+
+    public void onClickDeleteAllData(View view) {
         try {
             Uri cpUriDelete = Uri.parse(AUTHORITY + "/[" + mContext.getPackageName() + "]");
 
-            String whereClause = COLUMN_TARGET_APP_PACKAGE + " = '" + mContext.getPackageName() + "'" + " AND " + COLUMN_DATA_PERSIST_REQUIRED + " = '" + persisFlagSpinner.getSelectedItem().toString() + "'";
-            int i = getContentResolver().delete(cpUriDelete, whereClause, null);
-            if (i > 0) {
-                Toast.makeText(mContext, "User Info Deleted", Toast.LENGTH_SHORT).show();
-                resultView.setText("No Records Found");
-            } else {
-                Toast.makeText(mContext, "No User Info to be deleted", Toast.LENGTH_SHORT).show();
-            }
+            String whereClauseNotPersisted = COLUMN_TARGET_APP_PACKAGE + " = '" + mContext.getPackageName() + "'" + " AND " + COLUMN_DATA_PERSIST_REQUIRED + " = 'false'";
+            String whereClauseIsPersisted = COLUMN_TARGET_APP_PACKAGE + " = '" + mContext.getPackageName() + "'" + " AND " + COLUMN_DATA_PERSIST_REQUIRED + " = 'false'";
+            timeBegin= System.currentTimeMillis();
+            int rc_np = getContentResolver().delete(cpUriDelete, whereClauseNotPersisted, null);
+            int rc_ip = getContentResolver().delete(cpUriDelete, whereClauseIsPersisted, null);
+            long timeEnd = System.currentTimeMillis();
+            totalTimesec = (1. * timeEnd - 1. * timeBegin) / 1000.0;
+            resultView.setText("Deleted: "+rc_np+" non-persisted records\nDeleted: "+rc_ip+" persisted records");
         } catch (Exception e) {
             Log.d(TAG, "Delete - error: " + e.getMessage());
         }
